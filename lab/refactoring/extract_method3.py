1ae8924cb8d87baedaab500be36e15d0c2ce5c27
Lukaz <lukazdane@aol.com>
2021-03-03 16:14:39 -0800

    Update extract_method3.py

--- lab/refactoring/extract_method3.py
+++ lab/refactoring/extract_method3.py
@@ -16,5 +16,6 @@ ya = 97
 xb = .34
 yb = .91
 # calcualte the length of vector AB vector which is a vector between A and B points.
-length = math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
+length = length(xa, ya, xb, yb)
+# length = math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
 print('length', length)
